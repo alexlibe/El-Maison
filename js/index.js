afc83c99d3f9fa7ea95ae97fed69bde36d46522b
Alex Libertowski <alexski1029@gmail.com>
2020-12-17 18:16:54 -0500

    Added dynamic date/time

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
+let dateElement;
+let timeElement;
+
 function StartTime(){
+	const ONE_SECOND_IN_MILLISECONDS = 1000
 	let date = new Date();
-	let dateElement = document.getElementById("date");
-	let timeElement = document.getElementById("time");
 	
+	dateElement = document.getElementById("date");
+	timeElement = document.getElementById("time");
+	dateElement.innerText = date.toLocaleDateString();
+	timeElement.innerText = date.toLocaleTimeString();
+	
+	setInterval(SetDateTime, ONE_SECOND_IN_MILLISECONDS)
+}
+
+function SetDateTime(){
+	let date = new Date();
 	dateElement.innerText = date.toLocaleDateString();
 	timeElement.innerText = date.toLocaleTimeString();
 }
